971fcf3d2926f2236b7e5008cc66df7c2cc3ea0a
Pontes Neto <pontesneto2@hotmail.com>
2022-10-22 18:58:55 -0300

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+// import Button from "./components/Button"
+import React from "react"
 
-function App() {
+export default function App(){
+  const [items, setItems] = React.useState(0);
+  const [amount, setAmount] = React.useState(0);
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(() =>{
+    fetch ('http://192.168.3.112:8000/produtos')
+    .then(res => res.json())
+    .then (dados => setProducts(dados));
+  },[]);
+
+  const add = (price) => {
+    setItems(items + 1);
+    setAmount(amount + price);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+<div className="container">
 
-export default App;
+    <h1 className="mt-3">Carrinho de Compras</h1>
+    <span className="text-primary ms-3">{items}</span>
+    <span className="text-primary ms-3">{amount}</span>
+  <hr/>
+    {/* Comunicação com a api */}
+    {products.length} Produtos Encontrados no carrinho (Api)
+  <hr/>
+
+    {products.map(cadaProduto => {
+      return (
+        <div>
+        <span className="me-3">{cadaProduto.nome}</span>
+        <button className="btn btn-success btn-sm" onClick={() =>
+        add(cadaProduto.valor)}>Adicionar</button>
+        </div>
+      )
+    })}
+
+  <div>
+    <span className="me-3">Teclado R$32,00</span>
+    <button className="btn btn-success" onClick={() => add (32)}>Adicionar ao carrinho</button>
+  </div>
+
+  <div>
+    <span className="me-3">Mouse R$20,00</span>
+    <button className="btn btn-success" onClick={() => add (20)}>Adicionar ao carrinho</button>
+  </div>
+
+</div>
+  )
+}
